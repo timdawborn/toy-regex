214289b3857745781008acbf8c3a5f71d3941922
Tim Dawborn <tim.dawborn@airtasker.com>
2021-10-05 17:50:09 +1100

    Update tests to be in test form.

--- test_regex.py
+++ test_regex.py
@@ -1,8 +1,17 @@
+import unittest
+
 import regex
 
 
-TESTS = ['ae', 'adddde', 'abce', 'abcdddddbcddde', 'chicken', 'abde', 'a', '', 'abcd']
+class TestRegex(unittest.TestCase):
+    r = regex.compile(r'a(bc|d*)*e')
 
-r = regex.compile(r'a(bc|d*)*e')
-for test in TESTS:
-    print(r.accepts(test))
+    def test_positive_examples_from_lab_sheet(self):
+        TESTS = ['ae', 'adddde', 'abce', 'abcdddddbcddde']
+        for test in TESTS:
+            self.assertTrue(self.r.accepts(test))
+
+    def test_negative_examples_from_lab_sheet(self):
+        TESTS = ['chicken', 'abde', 'a', '', 'abcd']
+        for test in TESTS:
+            self.assertFalse(self.r.accepts(test))
